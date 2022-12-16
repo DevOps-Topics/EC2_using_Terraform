38e0cf0b08d5cc7ecdf2540359f60461d9bfa482
Rupert_VJG <62993058+DevOps-Topics@users.noreply.github.com>
2022-12-16 19:01:04 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,19 +1,17 @@
 variable "vpc_cidr" {
   description = "cidr for terraform vpc"
   type = string
-  default = 172.0.0.0/20
+  default = 192.168.0.0/20
 
 }
-variable "terraform_vpc_tags" {
+variable "my_vpc_tags" {
   description = "tags for terraform vpc"
   type = string
   default = terraform_vpc_tags
 
 }
 
-variable "vpc_id" {
-  default = aws_vpc.myvpc.id
-}
+variable "vpc_id" {}
 
 variable "subnet_cidr" {
   default = 172.0.1.0/24
